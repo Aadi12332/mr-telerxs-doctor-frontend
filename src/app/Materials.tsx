3ee6b96f336618e8bad6f90795a0d3bd6a71464c
adityaj698 <adityaj698@gmail.com>
2025-12-29 21:03:03 +0530

    communication layout, notification, material and report page design

--- src/app/Materials.tsx
+++ src/app/Materials.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import StatsOverview from "../components/cards/StatCard";
-import PlusIcon from "../assets/plusIcon.svg";
+import PlusIcon from "../assets/plusicon.svg";
 import type { StatItem } from "../components/cards/StatCard";
 import EyeIcon from "../assets/EyeIcon.svg";
 import CameraIcon from "../assets/cameraicon.svg";
